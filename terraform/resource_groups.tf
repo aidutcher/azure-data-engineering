66656ca9d7232779b9377f80143e2cc9abd4d70a
Alec Dutcher <aidutcher@yahoo.com>
2025-08-16 09:52:03 -0500

    Parameterize env across resources

--- terraform/resource_groups.tf
+++ terraform/resource_groups.tf
@@ -1,9 +1,9 @@
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
+  name     = "${var.env}${var.resource_group_name}"
   location = var.deployment_location
 
   tags = {
-    environment = "dev"
+    environment = var.env
   }
 }
